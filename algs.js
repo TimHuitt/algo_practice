de4a08bd2c2505fbeadff52c3c8ee0db5fc8e376
Tim Huitt <timhuitt@gmail.com>
2024-01-31 17:01:19 -0700

    complete challenge 3

--- algs.js
+++ algs.js
@@ -34,39 +34,89 @@ array = [13, 5, -4, 8, 11, 1, -1, 6]
 targetSum = 10
 
 // console.log(twoNumberSum(array, targetSum))
-_________________________________
+
+//_________________________________ //
 
 /// Determine if the second array is a subsequence of the first ///
 
 // first solution
+// function isValidSubsequence(array, sequence) {
+//   let arr = [...array]
+//   let seq = [...sequence]
+// 
+//   // loop sequence and compare each item to each array item
+//   while (seq) {
+// 
+//     // if items are equal
+//     // or seq item appears in array before next seq item
+//     if (arr[0] === seq[0]) {
+// 
+//       // continue
+//       seq.shift()
+//       arr.shift()
+// 
+//       // return true if all sequential matches found
+//       if (seq.length === 0) { return true }
+//     } else {
+//       arr.shift()
+//     }
+//     
+//     // return false if not found
+//     if (arr.length === 0) { return false}
+//   }
+// }
+
+// improved solution
 function isValidSubsequence(array, sequence) {
-  let arr = [...array]
-  let seq = [...sequence]
+  let seqIndex = 0;
 
-  // loop sequence and compare each item to each array item
-  while (seq) {
-
-    // if items are equal
-    // or seq item appears in array before next seq item
-    if (arr[0] === seq[0]) {
-
-      // continue
-      seq.shift()
-      arr.shift()
-
-      // return true if all sequential matches found
-      if (seq.length === 0) { return true }
-    } else {
-      arr.shift()
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === sequence[seqIndex]) {
+      seqIndex++
     }
-    
-    // return false if not found
-    if (arr.length === 0) { return false}
+    if (sequence.length === seqIndex) { return true }
   }
+  return false
 }
 
 array = [5, 1, 22, 25, 6, -1, 8, 10]
 sequence = [1, 6, -1, 10]
 
 // console.log(isValidSubsequence(array, sequence))
-_________________________________
+
+//_________________________________ //
+
+
+/// Replace the values of an ascending array with their square ///
+/// sorted ascending ///
+
+// first solution
+
+// function sortedSquaredArray(array) {
+//   return array.map((num) => num * num)
+//     .sort(function(a, b){return a-b})
+// }
+
+// improved solution
+
+function sortedSquaredArray(array) {
+  const squared = []
+  let left = 0
+  let right = array.length - 1
+
+  for (let i = array.length; i > 0; i--) {
+    if (Math.abs(array[left]) > Math.abs(array[right])) {
+      squared.push(array[left] * array[left])
+      left++
+    } else {
+      squared.push(array[right] * array[right])
+      right--
+    }
+  }
+  return squared
+}
+
+
+array = [1, 2, 3, 5, 6, 8, 9]
+
+// console.log(sortedSquaredArray(array))
