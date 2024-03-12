d6404354d8a0b24761404bd5048099851a1869c7
Tim Huitt <timhuitt@gmail.com>
2024-03-12 17:13:21 -0600

    start expression tree eval

--- algs.py
+++ algs.py
@@ -121,6 +121,56 @@ def nodeDepths(root):
 
 # _________________________________ //
 # _________________________________ //
+
+# take a binary tree and mathematically evaluate to return a single integer
+# all leaf nodes are positive integer operands
+# all branch nodes are negative integer operators
+# -1: addition
+# -2: subtraction
+# -3: division
+# -4: multiplication
+
+
+def evaluateExpressionTree(tree):
+  # create helper for recursive actions
+  # get operation sets
+    # deepest, leftmost ~ deepest rightmost
+  # replace set root with result
+  # repeat until left and right max depth is 1
+  # return final left and right result
+  sets = [[], []]
+
+  def getSets(node, sets, pos, depth):
+    if not node : return
+
+    if pos == 1:
+      sets[1].append(node.value)
+    elif pos == -1:
+      sets[0].append(node.value)
+    
+    getSets(node.left, sets, -1, depth)
+    getSets(node.right, sets, 1, depth)
+
+    depth += 1
+  def getResults(sets):
+    pos = 1 if sets[0][len(sets[0]) - 1] > 0 else -1
+    if all(not i for i in sets) : return
+
+    
+
+    return pos
+   
+  getSets(tree, sets, 0, 0)
+
+  print(getResults(sets))
+
+  return sets
+
+
+
+
+# _________________________________ //
+# _________________________________ //
 # Binary Tree Test Data
 
 class BST:
@@ -172,27 +222,28 @@ def buildBSTFromDict(nodeList):
 bst = {
   "tree": {
     "nodes": [
-      {"id": "1", "left": "2", "right": "8", "value": 1},
-      {"id": "2", "left": "3", "right": None, "value": 2},
-      {"id": "3", "left": "4", "right": None, "value": 3},
-      {"id": "4", "left": "5", "right": None, "value": 4},
-      {"id": "5", "left": "6", "right": None, "value": 5},
-      {"id": "6", "left": None, "right": "7", "value": 6},
-      {"id": "7", "left": None, "right": None, "value": 7},
-      {"id": "8", "left": None, "right": "9", "value": 8},
-      {"id": "9", "left": None, "right": "10", "value": 9},
-      {"id": "10", "left": None, "right": "11", "value": 10},
-      {"id": "11", "left": None, "right": "12", "value": 11},
-      {"id": "12", "left": "13", "right": None, "value": 12},
-      {"id": "13", "left": None, "right": None, "value": 13}
+      {"id": "1", "left": "2", "right": "3", "value": -1},
+      {"id": "2", "left": None, "right": None, "value": 2},
+      {"id": "3", "left": "4", "right": "5", "value": -2},
+      {"id": "4", "left": None, "right": None, "value": 4},
+      {"id": "5", "left": None, "right": None, "value": 5}
     ],
     "root": "1"
   }
 }
+
+  #   -1
+  #    /\
+  #   2  -2
+  #      /\
+  #     4  5
+
 
 
 # print(findClosestValueInBst(buildBSTFromDict(bst["tree"]["nodes"]), bst['target']))
 
 # print(branchSums(buildBSTFromDict(bst["tree"]["nodes"])))
 
-print(nodeDepths(buildBSTFromDict(bst["tree"]["nodes"])))
+# print(nodeDepths(buildBSTFromDict(bst["tree"]["nodes"])))
+
+print(evaluateExpressionTree(buildBSTFromDict(bst["tree"]["nodes"])))
