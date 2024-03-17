45f266d1e5315291096cad72ebaf4779ebe155f5
Tim Huitt <timhuitt@gmail.com>
2024-03-17 01:58:46 -0600

    add grid

--- react/components/src/components/App/App.jsx
+++ react/components/src/components/App/App.jsx
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import './App.css'
 
 function App() {
   const [ data, setData ] = useState(null)
-  const [ word, setWord ] = useState('')
-  
+  const [ error, setError ] = useState(null)
+  const [ word, setWord ] = useState('TESTING')
+  const [ boardData, setBoardData ] = useState([]) 
 
-  useEffect(() => {
-    const url = "/api/fe/wordle-words"
-    axios.get(url)
-      .then((res) => {
-        setData(res.data)
-      })
-      .catch((err) => {
-        setDefaultResultOrder(err.response ? err.response.data : err.message)
-      })
-
-      
-  }, [])
+//   useEffect(() => {
+//     const url = "/api/fe/wordle-words"
+//     axios.get(url)
+//       .then((res) => {
+//         setData(res.data)
+//       })
+//       .catch((err) => {
+//         console.error(err.response ? err.response.data : err.message)
+//       })
+// 
+//       
+//   }, [])
 
   useEffect(() => {
     if (data) {
@@ -26,9 +28,26 @@ function App() {
     }
   }, [data])
 
+  const line = (i) => {
+    return (
+      <div key={`line-${i}`} className="line">
+        {Array.from(Array(5).keys()).map((index) => (
+          <div className='tile'>
+            {`${i}-${index}`}
+          </div>
+        ))}
+      </div>
+    )}
+  
+  
+
   return (
-    <div className='App'>
+    <div className='board'>
       {word}
+      {Array.from(Array(6).keys()).map((index) => (
+        line(index)
+      ))}
+      <br />
     </div>
   )
 }
