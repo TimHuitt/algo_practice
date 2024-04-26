8cb862136023f81515ebb527c163c3752d305182
Tim Huitt <timhuitt@gmail.com>
2024-04-26 00:14:21 -0600

    update react connect

--- react/components/src/components/App/Connect/Connect.jsx
+++ react/components/src/components/App/Connect/Connect.jsx
@@ -5,17 +5,33 @@ export default function ConnectFour() {
   const [ winner, setWinner ] = useState(1)
   const [ restart, setRestart ] = useState(true)
   const [ board, setBoard ] = useState([])
+  const [ player, setPlayer ] = useState(1)
 
   useEffect(() => {
-    let b = Array(7).fill([])
-
-    b.map((col, i) => {
-      b[i] = Array(6).fill(0)
-    })
-
+    let b = Array(7).fill(null).map(() => Array(6).fill(0))
     setBoard(b)
-
   },[])
+
+  useEffect(() => {
+    setPlayer(prev => prev === 1 ? 2 : 1)
+  },[board])
+
+  const handleClick = (e) => {
+    const getRow = (col) => {
+      for (let i = 0; i < board[col].length; i++) {
+        if (board[col][i] !== 0) return i - 1
+      }
+      return 5
+    }
+
+    setBoard(currentBoard => {
+      const col = parseInt(e.target.id.slice(0,1))
+      const row = getRow(col)
+      const newBoard = currentBoard.map((c) => [...c])
+      newBoard[col][row] = player
+      return newBoard
+    })
+  }
 
 
   return (
@@ -25,9 +41,11 @@ export default function ConnectFour() {
       }
       <div className="board">
         { board.map((col, i) => (
-          <div className="column" key={`col-${i}`}>
+          <div className="column" id={`${i}`} key={`col-${i}`}>
             { col.map((cell, j) => (
-              <div className="tile" key={`col-${i}${j}`}>{i,j}</div>
+              <div className="tile" id={`${i}${j}`} key={`col-${i}${j}`} onClick={handleClick}>
+                  <div id={`${i}0`} className={`${board[i][j] === 1 ? "player player-1" : board[i][j] === 2 ? "player player-2" : ""}`} />
+              </div>
             ))}
           </div>
         ))}
